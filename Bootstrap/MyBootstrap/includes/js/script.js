4a4a19cc58bf6cb3d4c3d9eefa86757655a5fdd9
unknown <AY324230@L-777726117.wipro.com>
2015-11-06 22:04:18 +0530

    completed the website as part of Bootstrap video tutorial

--- Bootstrap/MyBootstrap/includes/js/script.js
+++ Bootstrap/MyBootstrap/includes/js/script.js
@@ -9,6 +9,14 @@ Notes:	 Hand coded for Udemy.com
 
 */
 $(function(){
-   
 
-})
+   $('#alertMe').click(function(e){
+     e.preventDefault();
+     $('#successAlert').slideDown();
+   });
+   $('a.pop').click(function(e){
+     e.preventDefault();
+   });
+  $('a.pop').popover();
+  $('[rel="tooltip"]').tooltip(); 
+});
