6ec71d82175c801b5e76c50eae6d3ae8f3c77e7c
therealayush <ayushjain.dce@gmail.com>
2016-01-17 01:31:15 +0530

    completed the authentication part

--- chirp/module-3/Start/app.js
+++ chirp/module-3/Start/app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var passport = require('passport');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var api = require('./routes/api');
-// var authenticate = require('./routes/authenticate');
+var authenticate = require('./routes/authenticate')(passport);
 
 var app = express();
 
@@ -16,14 +18,26 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
 app.use(logger('dev'));
+app.use(session({
+    secret : 'keyboard cat',
+    resave : true,
+    saveUninitialized : true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+//Initialize Passport
+var initPassport  = require('./passport-init');
+initPassport(passport);
 
 app.use('/api', api);
-//app.use('/auth', authenticate);
+app.use('/auth', authenticate);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
