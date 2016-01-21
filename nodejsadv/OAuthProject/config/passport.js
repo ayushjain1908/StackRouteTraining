cd59fc2681cbf3030787649d990c4e77dd481569
Ayush Jain <ayushjain.dce@gmail.com>
2016-01-21 10:27:00 +0530

    more oauth work

---
+++ nodejsadv/OAuthProject/config/passport.js
@@ -0,0 +1,21 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../app/models/user');
+
+module.exports = function(passport){
+
+  passport.serializeUser(function(user,done){
+      done(null,user.id);
+  });
+
+  passport.deserializeUser(function(id, done){
+    User.findById(id,function(err,user){
+      done(err,user);
+    });
+  });
+
+  passport.use('local-signup',new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
+  }))
+}
--- nodejsadv/OAuthProject/server.js
+++ nodejsadv/OAuthProject/server.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var session =  require('express-session');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
 var configDB = require('./config/database.js');
 mongoose.connect(configDB.url);
 app.use(morgan('dev'));
@@ -13,8 +15,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({secret : 'anystringoftext',
                 saveUninitialized : true,
                 resave : true}));
-
-app.set('view engine','ejs');                
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.set('view engine','ejs');
 /*
 app.use('/',function(req,res){
   res.send('Our first express program');
@@ -23,7 +27,7 @@ app.use('/',function(req,res){
   console.log(req.session);
 }); */
 
-require('./app/routes.js')(app);
+require('./app/routes.js')(app,passport);
 
 app.listen(3000);
 console.log("Server running on port 3000");
